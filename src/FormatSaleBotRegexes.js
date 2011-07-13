23d9f7b05f81bc890ccccb7c22879ec79b119355
Helder <he7d3r@gmail.com>
2011-07-13 02:51:04 -0300

    Add colors to Salebot rules
    Used on https://pt.wikipedia.org/wiki/User:Salebot/Config

---
+++ src/FormatSaleBotRegexes.js
@@ -0,0 +1,29 @@
+if ( mw.config.get( 'wgTitle' ) === 'Salebot/Config' && mw.config.get( 'wgAction' ) == 'view') {
+    $(function(){
+		var color = {
+			0 : 'transparent',
+			5 : '#CFC',
+			10 : '#6F6',
+			15 : '#FFC',
+			20 : '#FCA',
+			25 : '#F99',
+			30 : '#F66'
+		};
+		$('#bodyContent').find('pre').html(function(i,html){
+			return html.replace(
+				/([\-+#].+?) (\/.+?\/[gmi;]*) *(#.+)?/g,
+				function(match, p1, p2, p3){
+					var level = parseInt( p1, 10 );
+					if ( isNaN(level) ) {
+						return match;
+					} else {
+						level = Math.abs( level );
+						level = level - (level % 5);
+					}
+					return '<span style="background-color:' + color[ level] + ';">'
+						+ match + '</span>';
+				}
+			);
+		});
+	});
+}
