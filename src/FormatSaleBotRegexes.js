b4444242567650c25d19a60631e4aaac5770f4fc
Helder <he7d3r@gmail.com>
2012-11-05 10:51:20 +0000

    Minor changes

--- src/FormatSaleBotRegexes.js
+++ src/FormatSaleBotRegexes.js
@@ -4,40 +4,42 @@
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/FormatSaleBotRegexes.js]] ([[File:User:Helder.wiki/Tools/FormatSaleBotRegexes.js]])
  */
 /*jslint browser: true, white: true, regexp: true */
-/*global jQuery, mediaWiki */
-( function ( $, mw /* , undefined */ ) {
+/*global mediaWiki, jQuery*/
+( function ( mw, $ ) {
 'use strict';
 
 function formatSaleBotRegexes(){
 	var color = {
-		0 : 'transparent',
-		5 : '#CFC',
-		10 : '#6F6',
-		15 : '#FFC',
-		20 : '#FCA',
-		25 : '#F99',
-		30 : '#F66'
-	};
-	$('#bodyContent').find('pre').html(function(i,html){
-		return html.replace(
+			0 : 'transparent',
+			5 : '#CFC',
+			10 : '#6F6',
+			15 : '#FFC',
+			20 : '#FCA',
+			25 : '#F99',
+			30 : '#F66'
+		},
+		$pre = $('#mw-content-text').find('pre');
+	$pre.html(
+		$pre.html().replace(
 			// Ver linha 137 de [https://fisheye.toolserver.org/browse/gribeco/salebot2/branches/utf8/vandalism.pl?r=152]
 			/\s*([\-+#]?\d+)\s*(\/.+\/) *(#.+)?/g,
-			function(match, p1 /* , p2, p3 */ ){
+			function( match, p1 /* , p2, p3 */ ){
 				var level = parseInt( p1, 10 );
 				if ( isNaN(level) ) {
 					return match;
 				}
 				level = Math.abs( level );
 				level = level - (level % 5);
-				return '<span style="background-color:' + color[ level] + ';">' +
+				return '<span style="background-color:' +
+					color[ level ] + ';">' +
 					match + '</span>';
 			}
-		);
-	});
+		)
+	);
 }
 
 if ( mw.config.get( 'wgTitle' ) === 'Salebot/Config' && mw.config.get( 'wgAction' ) === 'view') {
 	$(formatSaleBotRegexes);
 }
 
-}( jQuery, mediaWiki ) );
+}( mediaWiki, jQuery ) );
